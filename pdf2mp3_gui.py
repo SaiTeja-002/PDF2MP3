d660ec5342904678969a7ef7a6d410036f32dad7
SaiTeja-002 <teja00219@gmail.com>
2022-12-23 23:11:00 +0530

    Implemented all the functionalities

--- pdf2mp3_gui.py
+++ pdf2mp3_gui.py
@@ -1,18 +1,19 @@
 import PyQt5.QtWidgets as qtw
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QFileDialog
 import PyQt5.QtGui as qtg
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt, QUrl
 import pyttsx3
 import PyPDF2 as pypdf
 import random
 import os
+import sys
 
 
 class MainWindow(qtw.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.resize(700, 700)
+        self.resize(1100, 500)
         labelFontSize = 13
         lineEditFontSize = 10
         comboBoxFontSize = 9
@@ -21,38 +22,36 @@ class MainWindow(qtw.QWidget):
         self.setWindowTitle("PDF2MP3")
         self.setLayout(qtw.QGridLayout())
 
-        # Logo
-        imageLabel = qtw.QLabel()
-        logoPixmap = QPixmap("Logo/logo_updated.png")
- 
-        # adding image to label
-        imageLabel.setPixmap(logoPixmap)
- 
-        # Optional, resize label to image size
-        imageLabel.resize(logoPixmap.width(), logoPixmap.height())
+        # Setting the logo as the icon
+        self.setWindowIcon(qtg.QIcon('Logo/logo_updated.png'))
 
-        # PDF Name Label
-        pdfNameLabel = qtw.QLabel("Enter the name of the PDF File")
-        pdfNameLabel.setFont(qtg.QFont("Helvetica", labelFontSize))
+        # Select PDF files
+        pdfFileSelectorButton = qtw.QPushButton("Select File", clicked=lambda:openFileSelector())
+        pdfFileSelectorButton.setFont(qtg.QFont("Helvetica", lineEditFontSize))
 
-        # MP3 Name Label
-        mp3NameLabel = qtw.QLabel("Generate the MP3 file as")
-        mp3NameLabel.setFont(qtg.QFont("Helvectica", labelFontSize))
+        # Select Directory and file Name
+        mp3FileSelectorButton = qtw.QPushButton("Select directory", clicked=lambda:getSaveFileName())
+        mp3FileSelectorButton.setFont(qtg.QFont("Helvetica", lineEditFontSize))
 
-        # PDF Name Entry Field
-        pdfNameEntryField = qtw.QLineEdit()
-        pdfNameEntryField.setObjectName("pdfName")
-        pdfNameEntryField.setFont(qtg.QFont("Helvetica", lineEditFontSize))
+        # PDF File Selector Entry Field (Un-Editable)
+        pdfFileSelectorLineEdit = qtw.QLineEdit()
 
-        # Completer for the pdfNameEntryField
-        completer = qtw.QCompleter(listOfPdfs)
-        pdfNameEntryField.setCompleter(completer)
+        pdfFileSelectorLineEdit.setStyleSheet("QLineEdit"
+                                            "{"
+                                            "background : gray;"
+                                            "}")
+  
+        pdfFileSelectorLineEdit.setReadOnly(True)
 
-        # MP3 Name Entry Field
-        mp3NameEntryField = qtw.QLineEdit()
-        mp3NameEntryField.setObjectName("mp3Name")
-        mp3NameEntryField.setFont(qtg.QFont("Helvetica", lineEditFontSize))
-        mp3NameEntryField.setText(pdfNameEntryField.text())
+        # Mp3 File Selector Entry Field
+        mp3FileSelectorLineEdit = qtw.QLineEdit()
+
+        mp3FileSelectorLineEdit.setStyleSheet("QLineEdit"
+                                            "{"
+                                            "background : gray;"
+                                            "}")
+    
+        mp3FileSelectorLineEdit.setReadOnly(True)
 
         # Assistant Label
         assistantLabel = qtw.QLabel("Choose your assistant")
@@ -75,40 +74,55 @@ class MainWindow(qtw.QWidget):
         speechRateEntryField.setFont(qtg.QFont("Helvetica", lineEditFontSize))
         speechRateEntryField.setText("130")
 
-        # Success Label
-        successLabel = qtw.QLabel("this shows the success of the operation")
-        successLabel.setFont(qtg.QFont("Helvetica", labelFontSize))
-        # successLabel.resize(20, 3)
-        successLabel.adjustSize()
-
         # Narrate Button to demonstrate the assistant's voice with the selected parameters
         narrateButton = qtw.QPushButton("Narrate", clicked=lambda:narrate())
         narrateButton.setFont(qtg.QFont("Helvetica", labelFontSize))
         narrateButton.setToolTip("Listen how the assistant sounds with the selected properties")
 
         # Create MP3 button
-        createMp3Button = qtw.QPushButton("Generate MP3", clicked=lambda:pdf2mp3())
+        createMp3Button = qtw.QPushButton("Create MP3", clicked=lambda:pdf2mp3())
         createMp3Button.setFont(qtg.QFont("Helvetica", labelFontSize))
 
         # Close Button
         closeButton = qtw.QPushButton("Close", clicked=lambda:terminateProgram())
         closeButton.setFont(qtg.QFont("Helvetica", labelFontSize))
 
-        # Adding widgets to the layout
-        self.layout().addWidget(pdfNameLabel,           0, 0)
-        self.layout().addWidget(pdfNameEntryField,      0, 1)
-        self.layout().addWidget(mp3NameLabel,           1, 0)
-        self.layout().addWidget(mp3NameEntryField,      1, 1)
-        self.layout().addWidget(assistantLabel,         2, 0)
-        self.layout().addWidget(assistantComboBox,      2, 1)
-        self.layout().addWidget(speechRateLabel,        3, 0)
-        self.layout().addWidget(speechRateEntryField,   3, 1)
-        # self.layout().addWidget(successLabel,           4, 0)
-        # self.layout().addWidget(qtw.QPushButton("Random"),           4, 1)
-        self.layout().addWidget(narrateButton,          5, 0)
-        self.layout().addWidget(createMp3Button,        5, 1)
-        self.layout().addWidget(closeButton,            6, 0, 1, 2)
 
+        self.layout().addWidget(pdfFileSelectorButton,    0, 0, 1, 1)
+        self.layout().addWidget(pdfFileSelectorLineEdit,  0, 1, 1, 1)
+        self.layout().addWidget(mp3FileSelectorButton,    1, 0, 1, 1)
+        self.layout().addWidget(mp3FileSelectorLineEdit,  1, 1, 1, 1)
+        self.layout().addWidget(assistantLabel,         2, 0, 1, 1)
+        self.layout().addWidget(assistantComboBox,      2, 1, 1, 2)
+        self.layout().addWidget(speechRateLabel,        3, 0, 1, 1)
+        self.layout().addWidget(speechRateEntryField,   3, 1, 1, 2)
+        self.layout().addWidget(narrateButton,          4, 0, 1, 1)
+        self.layout().addWidget(createMp3Button,        4, 1, 1, 1)
+
+        # Function to open the file explorer to select the desired directory
+        def openDirectorySelector():
+            global mp3Path
+            mp3Path = QFileDialog.getExistingDirectory(self, caption='Select a folder')
+            
+        # Function to open the file explorer and select the desired PDF file
+        def openFileSelector():
+            global pdfPath
+            fileName, _ = QFileDialog.getOpenFileName(self, "Select a PDF File", "", "PDF Files (*.pdf)")
+
+            if(fileName):
+                pdfPath = fileName
+
+            pdfFileSelectorLineEdit.setText(pdfPath)
+
+        # Function to open the file explorer and save the mp3 file at a desired location
+        def getSaveFileName():
+            global mp3Path
+            response, _ = QFileDialog.getSaveFileName(caption="Save as", filter="MP3 File (*.mp3)")
+            
+            if(response):
+                mp3Path = response
+
+            mp3FileSelectorLineEdit.setText(mp3Path)
 
         # Funciton to demonstrate the assistant's properties
         def narrate():
@@ -126,21 +140,17 @@ class MainWindow(qtw.QWidget):
 
         # Function to convert the PDF file into the MP3 file
         def pdf2mp3():
-            pdfName = pdfNameEntryField.text() + ".pdf"
-            mp3Name = mp3NameEntryField.text() + ".mp3"
-
-            pdfPath = "PDF Files/" + pdfName
-            mp3Path = "Audio Files/" + mp3Name
-
-            if(os.path.exists(pdfPath)):
-                # Read the pdf file and initialize the speaker
+            if(os.path.exists(pdfPath) and pdfPath.endswith(".pdf")):
+                
+                # Read the pdf file
                 pdfReader = pypdf.PdfFileReader(open(pdfPath, 'rb'))
 
-
                 # Extract data from each page and convert into a speakable form
+                data = ""
                 for pageNumber in range(pdfReader.numPages):
-                    data = pdfReader.getPage(pageNumber).extractText()
-                    formattedData = data.strip().replace("\n", "    ")
+                    data += pdfReader.getPage(pageNumber).extractText()
+                
+                formattedData = data.strip().replace("\n", "    ")
 
                 # Getting the properties of the assistant from their respective entry fields
                 assistantId = assistantComboBox.currentData()
@@ -157,18 +167,13 @@ class MainWindow(qtw.QWidget):
                 speaker.stop()
 
                 # Clearing the Entry Fields
-                pdfNameEntryField.setText("")
-                mp3NameEntryField.setText("")
-                successLabel.setText("Conversion successful")
+                pdfFileSelectorLineEdit.setText("")
+                mp3FileSelectorLineEdit.setText("")
                 speechRateEntryField.setText("130")
 
             else:
-                successLabel.setText("No such file found")
-                # def button_clicked(self, s):
-                # print("click", s)
-
                 errorDialog = ErrorDialog()
-                # errorDialog.setWindowTitle("Error")
+
                 errorDialog.exec()
 
         # Function to terminate the program
@@ -178,6 +183,7 @@ class MainWindow(qtw.QWidget):
         self.show()
 
 
+# A dialog box that pops up if the entered pdf path doesn't exist
 class ErrorDialog(QDialog):
     def __init__(self):
         super().__init__()
@@ -190,14 +196,48 @@ class ErrorDialog(QDialog):
         self.layout().addWidget(errorLabel)
 
 
+# Used to create drop-able line edits to simply drag and drop the PDF file
+class DropLineEdit(qtw.QLineEdit):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls:
+            event.accept()
+        else:
+            event.ignore()
+
+    def dragMoveEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.setDropAction(Qt.CopyAction)
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        if event.mimeData().hasUrls():
+            global droppedFile
+
+            event.setDropAction(Qt.CopyAction)
+            event.accept()
+
+            file = event.mimeData().urls()[0]
+            droppedFile = str(file.toLocalFile())
+
+            self.setText(droppedFile)
+
+        else:
+            event.ignore()
+
+
 speaker = pyttsx3.init()
 availableVoices = speaker.getProperty("voices")
 
-listOfPdfs = []
-
-for file in os.listdir("PDF Files"):
-    if file.endswith(".pdf"):
-        listOfPdfs.append(file[:-4])    # Removing the ".pdf" part because it is being added in the clicked method of generateMp3 button
+droppedFile = ""
+mp3Path = ""
+pdfPath = ""
 
 app = qtw.QApplication([])
 mainWindow = MainWindow()
--- tkintertest.py
+++ tkintertest.py
@@ -1,1 +1,17 @@
-import tkinter as tk
+path = "C:\Users\teja0\OneDrive - iiit-b\IIITB-OneDrive\PersonalProjects\PDF2MP3\PDF Files\Assignment-6.pdf"
+
+mp3Path = path[:-3]
+mp3Path += ".mp3"
+
+# path = path.replace("\\", "#")
+# lst = path.split("#")
+
+print(mp3Path)
+
+
+
+def downloadMP3():
+    fileName = QFileeDialog.getOpenFileName(self, "Open File", "", "MP3 Files (*.mp3)")
+
+    if(fileName):
+        print(str(fileName))
